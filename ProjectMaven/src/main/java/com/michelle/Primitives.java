6a3ec4a904a9f9786426e9dd99cc3e1b9f413bb0
Michelle Zou <michelle_zou@hcl.com>
2021-06-28 22:50:31 -0400

    test to see if git eclipse works

---
+++ ProjectMaven/src/main/java/com/michelle/Primitives.java
@@ -0,0 +1,18 @@
+package com.michelle;
+
+public class Primitives {
+
+	private byte Byte;
+	private short shorts;
+	private int integer;
+	private long Long;
+	private float Float;
+	private double Double;
+	private boolean bool;
+	private char Char;
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
