327316fac44c5968ce9ef7eb7991a2eb94fef0e7
michellezouHCL <86307270+michellezouHCL@users.noreply.github.com>
2021-06-28 22:09:35 -0400

    Updated ProjectMaven to include Exercise from Day2

---
+++ ProjectMaven/src/main/java/com/michelle/DateTest.java
@@ -0,0 +1,11 @@
+package com.michelle;
+
+public class DateTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Date newYear = new Date(1,1,2021);
+		newYear.displayDate(newYear.getMonth(), newYear.getDay(), newYear.getYear());
+	}
+
+}
