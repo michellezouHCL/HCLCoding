6b002829d8a368fe42a38eb7e9115f8e97d9b2c6
Michelle Zou <michelle_zou@hcl.com>
2021-06-29 12:24:45 -0400

    Updated Cycle, Bike, and InheritanceExample

---
+++ ProjectMaven/src/main/java/com/michelle/InheritenceExample.java
@@ -0,0 +1,8 @@
+package com.michelle;
+
+public class InheritenceExample {
+    public static void main(String []args){
+        Bike M = new Bike();
+        System.out.println(M.define_me());
+    }
+}
